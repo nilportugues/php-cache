f37dfbfe74148dc656d4aab416f91e75c92bd188
Nil Portugués Calderó <nilopc@gmail.com>
2015-03-25 01:20:14 +0100

    Update Cache.php

--- src/Cache.php
+++ src/Cache.php
@@ -1,7 +1,7 @@
 <?php
 namespace NilPortugues\Cache;
 
-use NilPortugues\Cache\Adapter\InMemoryCache;
+use NilPortugues\Cache\Adapter\InMemoryAdapter;
 
 /**
  * 
@@ -12,12 +12,12 @@ final class Cache implements CacheAdapter
 	* @var CacheAdapter  
 	*/
 	private $cache;
-
+	
 	/**
 	* @param CacheAdapter|null $cache
 	*/
 	public function __construct(CacheAdapter $cache = null)
 	{
-		$this->cache = (null === $cache) ? new InMemoryCache() : $cache;
+		$this->cache = (null === $cache) ? new InMemoryAdapter() : $cache;
 	}
 }
