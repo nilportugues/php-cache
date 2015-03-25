1dd9f154f0f314bd01d7c8eb5c72a2ec6c83bc2a
Nil Portugués Calderó <nilopc@gmail.com>
2015-03-25 01:35:49 +0100

    Update Cache.php

--- src/Cache.php
+++ src/Cache.php
@@ -14,10 +14,27 @@ final class Cache implements CacheAdapter
 	private $cache;
 	
 	/**
+	 * @var string
+	 */
+	private $namespace = '';
+	
+	/**
 	* @param CacheAdapter|null $cache
 	*/
-	public function __construct(CacheAdapter $cache = null)
+	public function __construct(CacheAdapter $cache = null, $namespace = '')
 	{
 		$this->cache = (null === $cache) ? new InMemoryAdapter() : $cache;
+		$this->namespace = (empty($namespace)) ? '' : $namespace.".";
+	}
+	
+	public function get($key)
+	{
+		return $this->cache->get($this->namespace.$key);
+	}
+	
+	public function set($key, $value, $ttl)
+	{
+		$this->cache->set($this->namespace.$key, $value, $ttl);
+		return $this;
 	}
 }
